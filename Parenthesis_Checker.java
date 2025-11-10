3377fb7964f4be0230dc8e3c618b37d7bd6460e5
haleyprindle <haley@prindle.net>
2025-11-10 19:26:22 +0000

    saddfghjkl

--- Parenthesis_Checker.java
+++ Parenthesis_Checker.java
@@ -1,18 +1,39 @@
-//
 //  Class author:  name of programmer
 //  Date created:  date that the class was initially created.
 //  General description: a brief summary of what this particular class does.
 //
 public class Parenthesis_Checker{
-public static void input(String s1)
+public static boolean isValid(String s1)
 {
-for(){
-
-}
-}
-
+for(int i=0;i<=s1.length()-1;i++){
+    String opening = s1.replaceFirst("[{[(]","");
+    int count = 0;
+    if(s1!=opening){
+        return false;
+    }
+    else
+    {
+     count++;
+    }
+    
     
 
+    String newString = s1.substring(1);
+
+    
+}
+}
 
 
+
+public static void main(String[] args){
+System.out.println(isValid("(){}[]")); //true
+System.out.println(isValid("(]")); //false
+System.out.println(isValid("([{({})}])")); //true
+System.out.println(isValid("(([]){})")); //true
+System.out.println(isValid("{}[())()(]")); //false
+System.out.println(isValid("(){}{"));  //false
+System.out.println(isValid("([)]")); //false
+System.out.println(isValid("([{}([)])]")); //false
+}
 }
