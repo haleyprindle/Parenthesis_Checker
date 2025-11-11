b4437bb0aeb3d17eb0dfa91de3e7deb63822d750
haleyprindle <haley@prindle.net>
2025-11-11 19:02:14 +0000

    done

--- Parenthesis_Checker.java
+++ Parenthesis_Checker.java
@@ -1,6 +1,6 @@
-//  Class author:  name of programmer
-//  Date created:  date that the class was initially created.
-//  General description: a brief summary of what this particular class does.
+//  Class author:  Haley Prindle
+//  Date created:  11/10/25
+//  General description: Given a String s1 containing just the characters '(', ')', '{', '}', '[' and ']', the program will determine if the input string is valid.
 //
 
 
@@ -13,7 +13,6 @@
 //  and open brackets must be closed in the correct order,then the program will return true. 
 //  If it's invalid, it will return false.
 //
-// 
 
 public class Parenthesis_Checker{
 public static boolean isValid(String s1)
@@ -28,12 +27,7 @@ public static boolean isValid(String s1)
     while(!s1.equals(newString));
     return s1.isEmpty();
 
-    
-
-    
 }
-
-
 
 public static void main(String[] args){
 System.out.println(isValid("(){}[]")); //true
