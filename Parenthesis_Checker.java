4a9ae555beed8dee6d37facfa6ad599237807e9e
haleyprindle <haley@prindle.net>
2025-11-11 00:35:41 +0000

    hbgfdzsfxgchv

--- Parenthesis_Checker.java
+++ Parenthesis_Checker.java
@@ -2,26 +2,35 @@
 //  Date created:  date that the class was initially created.
 //  General description: a brief summary of what this particular class does.
 //
+
+
+//
+//  Pre-condition: The input has to be the characters '(', ')', '{', '}', '[' and ']' 
+//  for the method to work.
+//  Post-condition: The result of this method is true or false being returned based on the input for s1
+//  If the input for s1 is valid, meaning open brackets must be closed by the same type of brackets,
+//  closed brackets have to have an open bracket pair of the same type, 
+//  and open brackets must be closed in the correct order,then the program will return true. 
+//  If it's invalid, it will return false.
+//
+// 
+
 public class Parenthesis_Checker{
 public static boolean isValid(String s1)
 {
-for(int i=0;i<=s1.length()-1;i++){
-    String opening = s1.replaceFirst("[{[(]","");
-    int count = 0;
-    if(s1!=opening){
-        return false;
-    }
-    else
-    {
-     count++;
-    }
-    
-    
-
-    String newString = s1.substring(1);
+ String newString;
+ do{
+    newString = s1;
+    s1 = s1.replace("()","")
+            .replace("{}","")
+            .replace("[]","");
+ }   
+    while(!s1.equals(newString));
+    return s1.isEmpty();
 
     
-}
+
+    
 }
 
 
